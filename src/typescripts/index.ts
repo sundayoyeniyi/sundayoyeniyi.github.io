89676ca76d961d5e1179de488b80ac997d044db2
sundayoyeniyi <sunday.oyeniyi@gmail.com>
2016-03-19 21:38:58 +0000

    cosmetics/formating update

--- src/typescripts/index.ts
+++ src/typescripts/index.ts
@@ -1,5 +1,7 @@
 import { DateUtility } from './util/DateUtility';
+
 const dateObjs: DateUtility.DateUtilities = new DateUtility.DateUtilities();
+
 window.onload = () => {
     let dateElement: any = document.querySelector('footer div:first-child');
     dateElement.innerText = dateObjs.formatCurrentDate(dateObjs.getCurrentDate());
