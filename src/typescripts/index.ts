bb03fc36832e42d4d81e30758cd1c2b1051f7452
sundayoyeniyi <sunday.oyeniyi@gmail.com>
2018-11-25 08:13:16 +0000

    Removing gulp & typescript artifacts
    These are no longer needed as I am now gradually migrating the profile page to react

--- src/typescripts/index.ts
+++
@@ -1,8 +0,0 @@
-import { DateUtility } from './util/DateUtility';
-
-const dateObjs: DateUtility.DateUtilities = new DateUtility.DateUtilities();
-
-window.onload = () => {
-    let dateElement: any = document.querySelector('footer div:first-child');
-    dateElement.innerText = dateObjs.formatCurrentDate(dateObjs.getCurrentDate());
-};
