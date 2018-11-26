bec265e199eab2bfaeed93e5d852b5c3a6603679
sundayoyeniyi <sunday.oyeniyi@gmail.com>
2018-11-26 07:52:25 +0000

    Webpack config
    - added webpack config
    - babel config
    - eslint config

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: ['./src/index.js'],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader', 'eslint-loader'],
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader' },
+                    { loader: 'sass-loader', options: {
+                                "includePaths": [
+                                require('path').resolve(__dirname, 'node_modules')
+                                ]
+                            }
+                    }
+                ],
+            },
+            {
+                test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 100000,
+                    },
+                },
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx', '.scss'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'public'),
+        publicPath: '/',
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        hot: true,
+        allowedHosts: ['0.0.0.0', 'localhost'],
+        host: '0.0.0.0',
+        useLocalIp: true,
+        port: 3000,
+    },
+};
