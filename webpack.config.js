cd335d5cc2f941479807eaeb31076b74a37a6c7d
sundayoyeniyi <sunday.oyeniyi@gmail.com>
2018-11-26 10:21:56 +0000

    Refactor of sass styles
    - Remove dependence on compass, susy and breakpoint plugins
    - Use normalize.css instead of reset css
    - added sass mixins for font-face
    - removed mixins that are based on compass

--- webpack.config.js
+++ webpack.config.js
@@ -17,16 +17,7 @@ module.exports = {
             },
             {
                 test: /\.(scss|css)$/,
-                use: [
-                    { loader: 'style-loader' },
-                    { loader: 'css-loader' },
-                    { loader: 'sass-loader', options: {
-                                "includePaths": [
-                                require('path').resolve(__dirname, 'node_modules')
-                                ]
-                            }
-                    }
-                ],
+                use: ['style-loader', 'css-loader','sass-loader'],
             },
             {
                 test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
